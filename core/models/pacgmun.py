24657456fc7eee34bf101cc7da754a7cda0fa735
Sergio Alejandro <sergioromero2k@gmail.com>
2026-08-18 16:21:17 +0200

    feat(pacgmun): finished class pacgmun constructer and functions

--- core/models/pacgmun.py
+++ core/models/pacgmun.py
@@ -1,23 +1,25 @@
-
 class Pacgum:
     """Represents a collectible dot in the maze."""
+
     def __init__(self,
                  pos: tuple[int, int],
-                 was_collected: bool,
                  point_value: int,
                  is_super: bool
                  ) -> None:
-        """Initializes a pacgmun at a given position with its point value."""
+        """Initializes a pacgum at a given position with its point value."""
         self.pos: tuple[int, int] = pos
         self.was_collected: bool = False
         self.point_value: int = point_value
         self.is_super: bool = is_super
 
     def mark_as_collected(self) -> bool:
-        """Marks the pacgmun as collected. 
-            Returns False if already collected."""
-        ...
+        """Marks the pacgum as collected.
+        Returns False if already collected."""
+        if self.was_collected:
+            return False
+        self.was_collected = True
+        return True
 
     def get_point_value(self) -> int:
-        """Returns the point value of this pacgmun."""
-        ...
+        """Returns the point value of this pacgum."""
+        return self.point_value
